2522b38e9d17c2026be5266ada3cb9ba314bc22e
MikeDriel <94164663+MikeDriel@users.noreply.github.com>
2023-02-22 16:23:00 +0100

    Battleship game start

---
+++ WebApp/Models/Battleship.cs
@@ -0,0 +1,51 @@
+﻿namespace WebApp.Models
+{
+	public class Battleship
+	{
+		Playboard Playboard;
+
+		public Battleship(Player player, int length)
+		{
+			this.Playboard = new Playboard(player, length);
+		}
+	}
+
+	public class Playboard
+	{
+		public int[,] Size { get; set; }
+		public Player Player { get; set; }
+		public IDictionary<int, int> Hit { get; set; }
+        public IDictionary<int, int> Boats { get; set; }
+        public Playboard(Player player, int length)
+		{
+            Size = new int[length, length];
+            this.Player = Player;
+            Hit = new Dictionary<int, int>();
+            Boats = new Dictionary<int, int>();
+        }
+		
+		public void FillBoats()
+		{
+			
+		}
+	}
+	
+	public class Player
+	{
+		public int PlayerId;
+	}
+
+	public class Boat
+	{
+		public int Id { get; set; }
+		public int Size { get; set; }
+
+
+		Boat(int Coordinates, int BoatId)
+		{
+			this.Id = Id;
+			this.Size = Size;
+		}
+
+	}
+}

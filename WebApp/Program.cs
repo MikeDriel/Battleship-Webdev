3bd37d1999a25c5bebec9d08c705d3ab065f8692
Calcium <94164663+MikeDriel@users.noreply.github.com>
2023-03-28 21:27:07 +0200

    Security headers

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -3,6 +3,8 @@ using WebApp.Hubs;
 using Microsoft.EntityFrameworkCore;
 using WebApp.Data;
 using WebApp.Areas.Identity.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.CodeAnalysis.Differencing;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("WebAppContextConnection") ?? throw new InvalidOperationException("Connection string 'WebAppContextConnection' not found.");
@@ -29,12 +31,35 @@ builder.Services.AddHttpClient<ReCaptcha>(client =>
     client.BaseAddress = new Uri("https://www.google.com/recaptcha/api/siteverify");
 });
 
+//Builder
 var app = builder.Build();
 
+// CSP && headers
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Add("Content-Security-Policy",
+        "default-src 'self' wss: ws:;" +
+        "script-src 'self' 'unsafe-inline';" +
+        "style-src 'self' 'unsafe-inline'" +
+        "font-src 'self'" +
+        "img-src 'self' data:;"
+    );
+    context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+    context.Response.Headers.Add("X-Frame-Options", "DENY");
+    context.Response.Headers.Add("Referrer-Policy", "strict-origin-when-cross-origin");
+    context.Response.Headers.Remove("X-Powered-By");
+    await next();
+});
+
+
 app.UseHsts();
+
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
+
 app.UseWebSockets();
+
 app.UseRateLimiter();
 
 app.UseRouting();

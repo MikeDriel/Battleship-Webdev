e613619476757b9bbcf9b9f8c90f0a38698c2b87
Calcium <94164663+MikeDriel@users.noreply.github.com>
2023-03-28 21:21:35 +0200

    fix hsts

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -31,17 +31,7 @@ builder.Services.AddHttpClient<ReCaptcha>(client =>
 
 var app = builder.Build();
 
-
-
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
-}
-
+app.UseHsts();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseWebSockets();

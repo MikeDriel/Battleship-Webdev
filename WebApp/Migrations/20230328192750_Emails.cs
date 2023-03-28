293d6f1074fc7020b467e7a08ac7d3ad59333300
Calcium <94164663+MikeDriel@users.noreply.github.com>
2023-03-28 21:28:31 +0200

    Email migration

---
+++ WebApp/Migrations/20230328192750_Emails.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Emails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Emails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmailAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmailBody = table.Column<string>(type: "nvarchar(600)", maxLength: 600, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Emails", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "b74ddd14-6340-4840-95c2-db12554843e5",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "c8336b2f-d9c3-46f1-a133-06a4efbabb0f", "AQAAAAIAAYagAAAAEGt/Q7mSghoOfEFP+WxBpKatSNLQTtrB/5QKZMChQ5ls7rxhVdAO4iaXwJ9OUeskAg==", "240a3886-9d44-415d-8d7e-57da90eec450" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Emails");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "b74ddd14-6340-4840-95c2-db12554843e5",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "c0b045f9-ad8e-4bc5-96fb-60e61ae1c5b5", "AQAAAAIAAYagAAAAEKL0DmZ9w1dnXQ72AEWSWNbZ3Q83aeqlVPq45ZdSfU/SyFcv4R7112A6C0Oe/ZkX7w==", "a6e070ac-5259-455b-88e1-74e139129f3f" });
+        }
+    }
+}

18bf73d0636050196a64402824242f0774d9ad0b
MikeDriel <94164663+MikeDriel@users.noreply.github.com>
2023-04-04 11:01:30 +0200

    Inline spain

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -38,13 +38,13 @@ var app = builder.Build();
 // CSP && headers
 app.Use(async (context, next) =>
 {
-    context.Response.Headers.Add("Content-Security-Policy",
-        "default-src 'self' wss: ws:;" +
-        "script-src 'self' 'unsafe-inline';" +
-        "style-src 'self' 'unsafe-inline'" +
-        "font-src 'self'" +
-        "img-src 'self' data:;"
-    );
+    //context.Response.Headers.Add("Content-Security-Policy",
+    //    "default-src 'self' wss: ws:;" +
+    //    "script-src 'self' 'unsafe-inline';" +
+    //    "style-src 'self' 'unsafe-inline'" +
+    //    "font-src 'self'" +
+    //    "img-src 'self' data:;"
+    //);
     context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
     context.Response.Headers.Add("X-Frame-Options", "DENY");
     context.Response.Headers.Add("Referrer-Policy", "strict-origin-when-cross-origin");
